a4c7bf663500f654d3c339a1ce31c9f0ff8644f0
Gabriel Acosta <gabriacos@gmail.com>
2024-08-10 13:58:50 -0300

    feat: :sparkles: INICIO del backend

--- src/components/TableIngresos.tsx
+++ src/components/TableIngresos.tsx
@@ -4,7 +4,7 @@ import React, { useState } from "react";
 const TableIngresos: React.FC = () => {
   const [ingresos, setIngresos] = useState([
     {
-      habitacion: "123",
+
       nombreApellido: "ALARCON AGOSTINA",
       dni: "38.236.296",
       ingreso: "28/07/24",
@@ -14,7 +14,6 @@ const TableIngresos: React.FC = () => {
       celular: "",
     },
     {
-      habitacion: "211",
       nombreApellido: "ALVAREZ EDUARDO ANDRES",
       dni: "17.224.605",
       ingreso: "28/07/24",
@@ -31,7 +30,7 @@ const TableIngresos: React.FC = () => {
       <table className="min-w-full divide-y divide-gray-200">
         <thead className="bg-slate-800">
           <tr>
-            <th className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider">Habitación</th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider">#</th>
             <th className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider">Nombre y Apellido</th>
             <th className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider">DNI N°</th>
             <th className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider">Ingreso</th>
@@ -44,7 +43,7 @@ const TableIngresos: React.FC = () => {
         <tbody className="bg-white divide-y divide-gray-200">
           {ingresos.map((ingreso, index) => (
             <tr key={index} className="hover:bg-gray-100">
-              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-700">{ingreso.habitacion}</td>
+              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-700">{index + 1}</td>
               <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-700">{ingreso.nombreApellido}</td>
               <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-700">{ingreso.dni}</td>
               <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-700">{ingreso.ingreso}</td>
